0fe00c36f81eaaaa66963d66c0eec556e702b2e7
vandos <wtf735@gmail.com>
2015-06-16 23:11:39 +0300

    model1

---
+++ ToDoTask/Context/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoTask.Context.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context dbContext;
+
+        public UnitOfWork(Context dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public DbSet<TEntity> GetEntitySet<TEntity>() where TEntity : class
+        {
+            return dbContext.Set<TEntity>();
+        }
+    }
+
+    public interface IUnitOfWork
+    {
+        void SaveChanges();
+        DbSet<TEntity> GetEntitySet<TEntity>() where TEntity : class;
+    }
+}
